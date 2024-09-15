dab9f96695e447c9e7de7391c982b50c0b2a40a3
Matheus Canovas <matheuscanovas9@gmail.com>
2024-09-15 18:36:20 -0300

    implementando controller canonical

--- Controllers/CanonicalController.cs
+++ Controllers/CanonicalController.cs
@@ -1,15 +1,29 @@
 using System;
+using ConnectData.Api.Data.Contexts;
+using ConnectData.Api.Data.Extensionsl;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConnectData.Api.Controllers;
 
 [ApiController]
 [Route("/v1/canonical")]
-public class CanonicalController : ControllerBase
+public class CanonicalController(ConnectDataContext context) : ControllerBase
 {
+
+    private readonly ConnectDataContext _context = context;
+
+
     [HttpGet("{produto:string}/{id:int}")]
     public ActionResult GetClienteCanonicalDataFromProduct(string produto, int id)
     {
-        return Ok();
+        try
+        {
+            var context = DbContextExtensions.GetDbSet(_context, produto);
+            var result = context.Find(id);
+            return Ok(result);
+        } catch(Exception ex)
+        {
+            return BadRequest(new { success = false, message = ex.Message });
+        }
     }
 }
